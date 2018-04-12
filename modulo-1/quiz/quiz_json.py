f90a332c8587b95c14342a51d79b76c7a5ad7948
marcusseabra <seabra.marcus@gmail.com>
2018-04-12 13:53:17 -0300

    Exercicio Json concluido

--- modulo-1/quiz/quiz_json.py
+++ modulo-1/quiz/quiz_json.py
@@ -26,7 +26,7 @@ def query_site(url, params, uid="", fmt="json"):
     """
     params["fmt"] = fmt
     r = requests.get(url + uid, params=params)
-    print "requesting", r.url
+    print ("requesting " + r.url)
 
     if r.status_code == requests.codes.ok:
         return r.json()
@@ -49,9 +49,9 @@ def pretty_print(data, indent=4):
     readable.
     """
     if type(data) == dict:
-        print json.dumps(data, indent=indent, sort_keys=True)
+        print (json.dumps(data, indent=indent, sort_keys=True))
     else:
-        print data
+        print (data)
 
 
 def main():
@@ -75,16 +75,71 @@ def main():
         if lstArtistas[i]['name'].upper() == "FIRST AID KIT":
             count = count + 1
 
-    print "How many bands named First Aid Kit?",
-    print count
+    print ("How many bands named First Aid Kit? " + str(count))
 
     results = query_by_name(ARTIST_URL, query_type["simple"], "Queen")
-#    pretty_print(results)
+    lstArtistas = results['artists']
+
     for i in range(len(lstArtistas)):
         if lstArtistas[i]['name'].upper() == "QUEEN":
-            print "Begin-area name for Queen?",
-            print lstArtistas[i]['begin-area']['name']
-            pretty_print(lstArtistas[i])
+            if("begin-area" in lstArtistas[i]):
+                print ("Begin-area name for Queen? " +lstArtistas[i]['begin-area']['name'])
+
+    results = query_by_name(ARTIST_URL, query_type["simple"], "Beatles")
+    lstArtistas = results['artists']
+
+    for i in range(len(lstArtistas)):
+        if lstArtistas[i]['name'].upper() == "THE BEATLES":
+            artist_id = lstArtistas[i]["id"]
+            dctRetorno = query_site(ARTIST_URL, query_type["aliases"], artist_id)
+            lstAliases = dctRetorno["aliases"]
+
+            for j in range(len(lstAliases)):
+                if "locale" in lstAliases[j] and "name" in lstAliases[j]:
+                    if lstAliases[j]['locale'] == "es":
+                        print("Spanish alias for Beatles? " + lstAliases[j]['name'])
+
+    results = query_by_name(ARTIST_URL, query_type["simple"], "Nirvana")
+    lstArtistas = results['artists']
+
+    for i in range(len(lstArtistas)):
+        if "tags" in lstArtistas[i]:
+            lstTags = lstArtistas[i]['tags']
+            for j in range(len(lstTags)):
+                if lstTags[j]['name'].upper() == "KURT COBAIN":
+                    print("Nirvana disambiguation? " + lstArtistas[i]['disambiguation'])
+
+    results = query_by_name(ARTIST_URL, query_type["simple"], "One Direction")
+    lstArtistas = results['artists']
+
+    for i in range(len(lstArtistas)):
+        if lstArtistas[i]['name'].upper() == "ONE DIRECTION":
+            if "life-span" in lstArtistas[i]:
+                 print("When was One Direction formed? " + lstArtistas[i]['life-span']['begin'])
+
+
+
+
+
+            #    print(lstAliases(j))
+            #for j in lstAliases:
+            #    print (lstAliases[j])
+            #for j in range(len(lstAliases)):
+            #    pretty_print(lstAliases[j])
+                #if "locale" in lstAliases[j]:
+                #    if lstAliases[j]['locale'].upper() == "ES":
+                #        print ("Spanish alias for Beatles?" + lstAliases[j]['locale'])
+
+            #pretty_print(artist_data)
+            #b10bbbfc-cf9e-42e0-be17-e2c3e1d2600d
+#            if("begin-area" in lstArtistas[i]):
+#                print ("Begin-area name for Queen? " +lstArtistas[i]['begin-area']['name'])
+
+#    pretty_print(results)
+
+# Spanish alias for Beatles?
+# Nirvana disambiguation?
+# When was One Direction formed?
 
     #pretty_print(results)
 
