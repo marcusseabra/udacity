844f753d4304fc91d9412bd49988554646b7ba87
Marcus Seabra <seabra.marcus@gmail.com>
2018-06-06 23:50:50 -0300

    Validade de dados

--- modulo-1/quiz/quiz_validade_dados.py
+++ modulo-1/quiz/quiz_validade_dados.py
@@ -18,16 +18,34 @@ You can write helper functions for checking the data and writing the files, but 
 """
 import csv
 import pprint
+import re
 
 INPUT_FILE = 'autos.csv'
 OUTPUT_GOOD = 'autos-valid.csv'
 OUTPUT_BAD = 'FIXME-autos.csv'
+ANO_INICIAL = 1886
+ANO_FINAL = 2014
 
 def process_file(input_file, output_good, output_bad):
 
     with open(input_file, "r") as f:
         reader = csv.DictReader(f)
         header = reader.fieldnames
+        # Recurso de expressao regular para busca de DBPedia em strings de interesse
+        p = re.compile('DBPedia', re.IGNORECASE)
+        for linhas in reader:
+            # Verifica a existencia da expressao regular na string, no caso, a coluna URI
+            # Busca pela string DBPedia no conteudo da coluna
+            if p.search(linhas["URI"]):
+                ano = str(linhas["productionStartYear"])
+                # Valida a existencia de um numero (ano) na string
+                n = re.compile('\d')
+                if n.match(ano[:4]):
+                    ano_interesse = int(ano[:4])
+                    if ano_interesse >= ANO_INICIAL and ano_interesse <= ANO_FINAL:
+                        print(linhas)
+#                n = re.compile('DBPedia')
+#                print(linhas["productionStartYear"])
 
         #COMPLETE THIS FUNCTION
 
@@ -35,12 +53,13 @@ def process_file(input_file, output_good, output_bad):
 
     # This is just an example on how you can use csv.DictWriter
     # Remember that you have to output 2 files
+    '''
     with open(output_good, "w") as g:
         writer = csv.DictWriter(g, delimiter=",", fieldnames= header)
         writer.writeheader()
         for row in YOURDATA:
             writer.writerow(row)
-
+    '''
 
 def test():
 
