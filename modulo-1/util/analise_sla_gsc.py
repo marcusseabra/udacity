a22c88db82b36e63a071fc515b6b62a06bdba6a9
Marcus Seabra <seabra.marcus@gmail.com>
2018-06-14 06:09:45 -0300

    SLA GSC

--- modulo-1/util/analise_sla_gsc.py
+++ modulo-1/util/analise_sla_gsc.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import re
 import csv
@@ -8,6 +9,7 @@ DATADIR_DESTINO='/home/seabra/dev/udacity/modulo-1/'
 
 ARQUIVO_TRANSICAO_RMS = "IND - Transicao RMs.csv"
 ARQUIVO_RESPONSAVEIS_RMS = "IND - Responsaveis.csv"
+FORMATO_DATA_HORA = "%Y-%m-%d %H:%M:%S"
 
 def leitura_transicao():
     transicoes_rms = []
@@ -20,9 +22,28 @@ def leitura_transicao():
     arquivo_csv.close()
     return transicoes_rms
 
-#    for rm in transicoes_rms:
-#        if rm['rm'] == "13593":
-#            print("Situacao Atual: " + rm['situacao'] + " - Transicao: " + rm['data_transicao'])
+def evolucao_rms():
+    lista_tempo_espera = []
+    transicoes_rms = leitura_transicao()
+    rm_anterior = 0
+    for transicoes in transicoes_rms:
+        rm_atual = transicoes['rm']
+        if transicoes['situacao_atual'] == "Homologada" or transicoes['situacao_atual'] == "Concluída":
+            if rm_atual != rm_anterior:
+                tempo_espera = {}
+                dataHoraCriacao = datetime.strptime(transicoes['criacao_rm'], FORMATO_DATA_HORA)
+                dataHoraInicial = datetime.strptime(transicoes['transicao'], FORMATO_DATA_HORA)
+                delta = dataHoraInicial - dataHoraCriacao
+                tempo_espera['rm'] = rm_atual
+                tempo_espera['tempo'] = delta.days
+                lista_tempo_espera.append(tempo_espera)
+                rm_anterior = rm_atual
+                transicao_inicial = True
+                tempo_transicao = 0
+            elif rm_atual == rm_anterior and transicao_inicial:
+                dataHoraAtual = datetime.strptime(transicoes['transicao'], FORMATO_DATA_HORA)
+                delta = dataHoraAtual - dataHoraInicial
+                tempo_transicao = delta.days
 
 def definicao_responsaveis():
     transicoes_rms = leitura_transicao()
@@ -54,4 +75,4 @@ def definicao_responsaveis():
                                 + " - "
                                 + responsaveis['data_atribuicao'])
 
-definicao_responsaveis()
+evolucao_rms()
